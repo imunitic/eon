4ba7943b1ebf040642221bfd9337eedc338feccc
Ivica Munitić <imunitic@gmail.com>
2025-10-15 18:38:27 +0200

    [eon :: ecs] Signals, Events, Commands should be public

--- eon_ecs/eon_ecs.ml
+++ eon_ecs/eon_ecs.ml
@@ -1,3 +1,7 @@
 module Entity_id = Entity_id
 module Component = Component
 module World = World
+
+module Signals = Messaging.Signals
+module Events = Messaging.Events
+module Commands = Messaging.Commands
