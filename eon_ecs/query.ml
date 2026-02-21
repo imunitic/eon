6ef7bc2747b24594faf1269d58bdd5c42ae43a46
Ivica Munitić <imunitic@gmail.com>
2026-02-21 19:01:43 +0100

    [eon :: core] enable heterogeneous query iterator payload types

--- eon_ecs/query.ml
+++ eon_ecs/query.ml
@@ -35,64 +35,87 @@ let iter1 world c1 f =
     (storage world c1)
 
 (** Iterate over entities with two components. *)
-let iter2 world c1 c2 f =
+let iter2 (type a b) world c1 c2 (f : Entity_id.t -> a -> b -> unit) : unit =
   match storage world c1, storage world c2 with
   | Some s1, Some s2 ->
-     let base, _ =
-       if Sparse_set.size s1 < Sparse_set.size s2 then (s1, s2)
-       else (s2, s1)
+     let s1 = (Obj.magic s1 : a Sparse_set.t) in
+     let s2 = (Obj.magic s2 : b Sparse_set.t) in
+     let visit id =
+       let eid = eid_of world id in
+       match Sparse_set.get s1 eid, Sparse_set.get s2 eid with
+       | Some v1, Some v2 -> f eid v1 v2
+       | _ -> ()
      in
-     Sparse_set.iter
-       (fun id _ ->
-         let eid = eid_of world id in
-         match Sparse_set.get s1 eid, Sparse_set.get s2 eid with
-         | Some v1, Some v2 -> f eid v1 v2
-         | _ -> ())
-       base
+     if Sparse_set.size s1 < Sparse_set.size s2 then
+       Sparse_set.iter (fun id _ -> visit id) s1
+     else
+       Sparse_set.iter (fun id _ -> visit id) s2
   | _ -> ()
 
 (** Iterate over entities with three components. *)
-let iter3 world c1 c2 c3 f =
+let iter3 (type a b c) world c1 c2 c3 (f : Entity_id.t -> a -> b -> c -> unit) : unit =
   match storage world c1, storage world c2, storage world c3 with
   | Some s1, Some s2, Some s3 ->
-     let base =
-       smallest [ s1; s2; s3 ] |> Option.get
+     let s1 = (Obj.magic s1 : a Sparse_set.t) in
+     let s2 = (Obj.magic s2 : b Sparse_set.t) in
+     let s3 = (Obj.magic s3 : c Sparse_set.t) in
+     let visit id =
+       let eid = eid_of world id in
+       if Sparse_set.contains s1 eid
+          && Sparse_set.contains s2 eid
+          && Sparse_set.contains s3 eid
+       then
+         match Sparse_set.get s1 eid,
+               Sparse_set.get s2 eid,
+               Sparse_set.get s3 eid with
+         | Some v1, Some v2, Some v3 -> f eid v1 v2 v3
+         | _ -> ()
      in
-     Sparse_set.iter
-       (fun id _ ->
-         let eid = eid_of world id in
-         if Sparse_set.contains s1 eid
-            && Sparse_set.contains s2 eid
-            && Sparse_set.contains s3 eid
-         then
-           match Sparse_set.get s1 eid,
-                 Sparse_set.get s2 eid,
-                 Sparse_set.get s3 eid with
-           | Some v1, Some v2, Some v3 -> f eid v1 v2 v3
-           | _ -> ())
-       base
+     let n1 = Sparse_set.size s1
+     and n2 = Sparse_set.size s2
+     and n3 = Sparse_set.size s3 in
+     if n1 <= n2 && n1 <= n3 then
+       Sparse_set.iter (fun id _ -> visit id) s1
+     else if n2 <= n1 && n2 <= n3 then
+       Sparse_set.iter (fun id _ -> visit id) s2
+     else
+       Sparse_set.iter (fun id _ -> visit id) s3
   | _ -> ()
 
 (** Iterate over entities with four components *)
-let iter4 world c1 c2 c3 c4 f =
+let iter4 (type a b c d) world c1 c2 c3 c4 (f : Entity_id.t -> a -> b -> c -> d -> unit) : unit =
   match storage world c1, storage world c2, storage world c3, storage world c4 with
   | Some s1, Some s2, Some s3, Some s4 ->
-     let base = smallest [ s1; s2; s3; s4 ] |> Option.get in
-     Sparse_set.iter
-       (fun id _ ->
-         let eid = eid_of world id in
-         if Sparse_set.contains s1 eid
-            && Sparse_set.contains s2 eid
-            && Sparse_set.contains s3 eid
-            && Sparse_set.contains s4 eid
-         then
-           match Sparse_set.get s1 eid,
-                 Sparse_set.get s2 eid,
-                 Sparse_set.get s3 eid,
-                 Sparse_set.get s4 eid with
-           | Some v1, Some v2, Some v3, Some v4 -> f eid v1 v2 v3 v4
-           | _ -> ())
-       base
+     let s1 = (Obj.magic s1 : a Sparse_set.t) in
+     let s2 = (Obj.magic s2 : b Sparse_set.t) in
+     let s3 = (Obj.magic s3 : c Sparse_set.t) in
+     let s4 = (Obj.magic s4 : d Sparse_set.t) in
+     let visit id =
+       let eid = eid_of world id in
+       if Sparse_set.contains s1 eid
+          && Sparse_set.contains s2 eid
+          && Sparse_set.contains s3 eid
+          && Sparse_set.contains s4 eid
+       then
+         match Sparse_set.get s1 eid,
+               Sparse_set.get s2 eid,
+               Sparse_set.get s3 eid,
+               Sparse_set.get s4 eid with
+         | Some v1, Some v2, Some v3, Some v4 -> f eid v1 v2 v3 v4
+         | _ -> ()
+     in
+     let n1 = Sparse_set.size s1
+     and n2 = Sparse_set.size s2
+     and n3 = Sparse_set.size s3
+     and n4 = Sparse_set.size s4 in
+     if n1 <= n2 && n1 <= n3 && n1 <= n4 then
+       Sparse_set.iter (fun id _ -> visit id) s1
+     else if n2 <= n1 && n2 <= n3 && n2 <= n4 then
+       Sparse_set.iter (fun id _ -> visit id) s2
+     else if n3 <= n1 && n3 <= n2 && n3 <= n4 then
+       Sparse_set.iter (fun id _ -> visit id) s3
+     else
+       Sparse_set.iter (fun id _ -> visit id) s4
   | _ -> ()
 
 (** Count how many entities match the given components. *)
